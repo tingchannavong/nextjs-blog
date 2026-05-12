2f69edeae1ee5786b28b0f36781df05342231792
tingchannavong <tingchannavong.gmail.com>
2026-05-12 15:04:05 +0700

    image next

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <section className={styles.hero}>
+      <div className="flex gap-2 justify-center">
+      <p>&lt;Image/&gt; Nextjs</p>
       <Image src="/images/dog.jpg" alt="The Black Dog" width={225} height={125}/>
-       {/* <img src="/images/dog.jpg" alt="dogpic" /> */}
+      <p>&lt;Img/&gt; HTML5</p>
+       <img src="./images/dog.jpg" alt="dogpic" />
+      </div>
       <div className={styles.heroContent}>
         <h1 className={styles.title}>/\ Welcome to <span className={styles.titleAccent}>Next JS</span> App /\</h1>
         <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero alias soluta dolor consequatur velit cum quo, blanditiis quam! Iure accusamus dolorem distinctio facilis corporis quidem, labore quam quas ullam sunt? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ipsa voluptatum, enim fuga provident id voluptates magnam labore architecto sit.</p>
